4ce4d4fb9cdf680847d54e3aac052733cd49c114
Guy Romm <guyromm@gmail.com>
2021-11-09 11:44:11 +0200

    schema updated after sql/tables.sql column renames.

--- sql/schema/attendees.TABLE.sql
+++ sql/schema/attendees.TABLE.sql
@@ -5,7 +5,7 @@ CREATE TABLE public.attendees (
     id integer NOT NULL,
     ts timestamp with time zone DEFAULT now(),
     attendee_source_id character varying NOT NULL,
-    owner character varying DEFAULT current_setting('request.jwt.claim.email'::text, true),
+    owner_id character varying DEFAULT current_setting('request.jwt.claim.email'::text, true),
     email character varying,
     first_name character varying,
     last_name character varying
